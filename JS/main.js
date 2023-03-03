f2c5c7213f3d37ac2a46439eb799f8a294123fd7
Victor Franciulli <victor.franciulli@gmail.com>
2023-03-02 23:30:15 -0300

    js code refactoring

--- JS/main.js
+++ JS/main.js
@@ -2,38 +2,28 @@
 const form = document.getElementById('novoItem');
 const lista = document.getElementById('lista');
 
-//Evento de interação com o usuário que recebe os dados passados utilizando inputs do projeto base:
-
-//pega o evento e para o comportamento padrão dele.
-//submit é um evento.
-//quando disparar o evento clicando em submit, vai executar uma função (evento) que para o comportamento padrão e pega o valor do elemento alvo daquele evento.
-
 form.addEventListener('submit', (evento) => {
     evento.preventDefault();
 
-    //cria o elemento novo com base nos valores dos campos de input quando o evento é disparado.
-    criaElemento(evento.target.elements['nome'].value, evento.target.elements['quantidade'].value);
+    const nome = evento.target.elements['nome'];
+    const quantidade = evento.target.elements['quantidade'];
 
-    //reseta os cmapos nome e quantidade após o submit
-    evento.target.elements['nome'].value = "";
-    evento.target.elements['quantidade'].value = "";
+    criaElemento(nome.value, quantidade.value);
+
+    nome.value = "";
+    quantidade.value = "";
 });
 
-//Função que recebe esses os dados de item e quantidade, e retorne adicionando-os na lista ul, estilizados com a tag strong.
-
-//para criar o elemento precisa receber os elementos nome e quantidade
 function criaElemento(nome, quantidade) {
 
-    const novoItem = document.createElement('li'); //cria o elemento HTML de lista
-    novoItem.classList.add('item'); //atribui uma classe ao elemento HTML criado
+    const novoItem = document.createElement('li');
+    novoItem.classList.add('item');
 
-    const numeroItem = document.createElement('strong'); //cria a tag strong
-    numeroItem.innerHTML = quantidade; //da o valor de quantidade como conteúdo da tag strong
+    const numeroItem = document.createElement('strong');
+    numeroItem.innerHTML = quantidade;
 
-    novoItem.appendChild(numeroItem); //cria um objeto NO HTML
-    novoItem.innerHTML += nome; // adiciona o nome após tag strong
-    
-    //const lista = document.getElementById('lista'); **pega o id de lista do HTML
+    novoItem.appendChild(numeroItem);
+    novoItem.innerHTML += nome;
 
-    lista.appendChild(novoItem); //cria um objeto NO HTML: <li>
+    lista.appendChild(novoItem);
 };
